609044772cfe4573986f29a51f8cc4e90c92968b
ramazanovkuat <rkm-kuat@mail.ru>
2022-10-24 03:30:13 +0300

    close_zero changed2

--- close_zero.py
+++ close_zero.py
@@ -1,6 +1,5 @@
 def get_closest_to_zero(a):
     abs_a = [abs(j) for j in a]
-    closest_val = min(abs_a)
     closest_zero_list = [val for i, val in enumerate(a) if abs_a[i] == min(abs_a)]
     return closest_zero_list
     
