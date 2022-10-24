045db55ea18a3aeca5724da5076614f9a041ae79
ramazanovkuat <rkm-kuat@mail.ru>
2022-10-24 03:59:44 +0300

    update merge_sequences

--- merge_sequences.py
+++ merge_sequences.py
@@ -1,12 +1,14 @@
 def get_merged_sequence(seq1, seq2):
     merged = []
-    i = j = 0
+    i = 0
+    j = 0
     while i < len(seq1) and j < len(seq2):
         if seq1[i] == seq2[j]:
             if len(merged) == 0 or merged[-1] != seq1[i]:
                 merged.append(seq1[i])
             i += 1
             j += 1
+            print(i, j)
         elif seq1[i] < seq2[j]:
             i += 1
         else:
